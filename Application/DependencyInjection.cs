9a88968ca6252f419fc788414ccc626fff58846e
Lasha Morgoshia <xnagamer@live.com>
2024-01-07 23:26:32 +0400

    Update

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,20 +1,19 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Application
+namespace Application;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
-            var assembly = typeof(DependencyInjection).Assembly;
+        var assembly = typeof(DependencyInjection).Assembly;
 
-            services.AddMediatR(config =>
-                config.RegisterServicesFromAssembly(assembly));
+        services.AddMediatR(config =>
+            config.RegisterServicesFromAssembly(assembly));
 
-            services.AddValidatorsFromAssembly(assembly);
+        services.AddValidatorsFromAssembly(assembly);
 
-            return services;
-        }
+        return services;
     }
 }
---
+++ WebApi/Program.cs
@@ -0,0 +1,31 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((ctx, conf) => conf.ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
